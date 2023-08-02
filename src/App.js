fdb14b7014cff3e3e55dbd83bacd9f835c347b8c
FERNANDA LOPEZ <soyfumii@gmail.com>
2023-08-02 10:13:12 -0700

    Agregado los botones generacionales

--- src/App.js
+++ src/App.js
@@ -24,5 +24,3 @@ function App() {
 }
 
 export default App;
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
--- src/Generations.js
+++ src/Generations.js
@@ -1,8 +1,88 @@
 function Generations() {
-  return <>
-   <h1>Generation 1</h1>
-  
-  </>;
+  return (
+    <>
+      <h1>Generation 1</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 2</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 3</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 4</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 5</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 6</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 7</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 8</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+      <br/>
+      <h1>Generation 9</h1>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Buscar
+      </button>
+    </>
+  );
 }
 
 export default Generations;
